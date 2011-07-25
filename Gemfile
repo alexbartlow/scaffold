27ddabcd7fb5677419225a2a698c3dbd5664e0e8
Alex Bartlow <bartlowa@gmail.com>
2011-07-24 23:04:52 -0400

    Scaffold for rails app

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3'
+
+gem 'sass'
+gem 'haml'
+gem 'jquery-rails'
+gem 'barista'
+gem 'therubyracer', :require => false
+gem 'factory_girl'
+gem 'nokogiri'
+gem 'jammit'
+group :development, :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backbone::Application.config.secret_token = '92975434dda541f19ea549f61a7222ccfbdf2ba4a83375553b9a04ab89f97c1635f355fe793fcecb97c4fee6ac24503298f176f63b5f6762be2a84d66ca7b0a2'
