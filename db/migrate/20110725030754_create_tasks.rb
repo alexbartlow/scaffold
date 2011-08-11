333ea58705eeeb0398cc28f29221a0f324ddc2dc
Alex Bartlow <bartlowa@gmail.com>
2011-08-10 20:43:50 -0400

    Add tasks

---
+++ db/migrate/20110725030754_create_tasks.rb
@@ -0,0 +1,15 @@
+class CreateTasks < ActiveRecord::Migration
+  def self.up
+    create_table :tasks do |t|
+      t.string :name
+      t.datetime :due_date
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :tasks
+  end
+end
